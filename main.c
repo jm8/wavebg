c7d552320c6cb69fc9d441c9dba2623445b09997
josh <jm8hjkl@gmail.com>
2023-02-04 15:54:40 -0500

    remove unneeded code

--- main.c
+++ main.c
@@ -16,24 +16,6 @@
 #include "viewporter-client-protocol.h"
 #include "single-pixel-buffer-v1-client-protocol.h"
 
-static uint32_t parse_color(const char *color) {
-	if (color[0] == '#') {
-		++color;
-	}
-
-	int len = strlen(color);
-	if (len != 6 && len != 8) {
-		swaybg_log(LOG_DEBUG, "Invalid color %s, defaulting to 0xFFFFFFFF",
-				color);
-		return 0xFFFFFFFF;
-	}
-	uint32_t res = (uint32_t)strtoul(color, NULL, 16);
-	if (strlen(color) == 6) {
-		res = (res << 8) | 0xFF;
-	}
-	return res;
-}
-
 struct swaybg_state {
 	struct wl_display *display;
 	struct wl_compositor *compositor;
@@ -41,25 +23,9 @@ struct swaybg_state {
 	struct zwlr_layer_shell_v1 *layer_shell;
 	struct wp_viewporter *viewporter;
 	struct wp_single_pixel_buffer_manager_v1 *single_pixel_buffer_manager;
-	struct wl_list configs;  // struct swaybg_output_config::link
 	struct wl_list outputs;  // struct swaybg_output::link
 	struct wl_list images;   // struct swaybg_image::link
 	bool run_display;
-};
-
-struct swaybg_image {
-	struct wl_list link;
-	const char *path;
-	bool load_required;
-};
-
-struct swaybg_output_config {
-	char *output;
-	const char *image_path;
-	struct swaybg_image *image;
-	enum background_mode mode;
-	uint32_t color;
-	struct wl_list link;
 };
 
 struct swaybg_output {
@@ -84,68 +50,22 @@ struct swaybg_output {
 	struct wl_list link;
 };
 
-bool is_valid_color(const char *color) {
-	int len = strlen(color);
-	if (len != 7 || color[0] != '#') {
-		swaybg_log(LOG_ERROR, "%s is not a valid color for swaybg. "
-				"Color should be specified as #rrggbb (no alpha).", color);
-		return false;
-	}
-
-	int i;
-	for (i = 1; i < len; ++i) {
-		if (!isxdigit(color[i])) {
-			return false;
-		}
-	}
-
-	return true;
-}
-
 static void render_frame(struct swaybg_output *output, cairo_surface_t *surface) {
 	int buffer_width = output->width * output->scale,
 		buffer_height = output->height * output->scale;
 
 	// If the last committed buffer has the same size as this one would, do
 	// not render a new buffer, because it will be identical to the old one
-	// if (output->committed_width == buffer_width &&
-	// 		output->committed_height == buffer_height) {
-	// 	if (output->committed_scale != output->scale) {
-	// 		wl_surface_set_buffer_scale(output->surface, output->scale);
-	// 		wl_surface_commit(output->surface);
+	if (output->committed_width == buffer_width &&
+			output->committed_height == buffer_height) {
+		if (output->committed_scale != output->scale) {
+			wl_surface_set_buffer_scale(output->surface, output->scale);
+			wl_surface_commit(output->surface);
 
-	// 		output->committed_scale = output->scale;
-	// 	}
-	// 	return;
-	// }
-
-	// if (output->config->mode == BACKGROUND_MODE_SOLID_COLOR &&
-	// 		output->state->viewporter &&
-	// 		output->state->single_pixel_buffer_manager) {
-	// 	uint8_t r8 = (output->config->color >> 24) & 0xFF;
-	// 	uint8_t g8 = (output->config->color >> 16) & 0xFF;
-	// 	uint8_t b8 = (output->config->color >> 8) & 0xFF;
-	// 	uint8_t a8 = (output->config->color >> 0) & 0xFF;
-	// 	uint32_t f = 0xFFFFFFFF / 0xFF; // division result is an integer
-	// 	uint32_t r32 = r8 * f;
-	// 	uint32_t g32 = g8 * f;
-	// 	uint32_t b32 = b8 * f;
-	// 	uint32_t a32 = a8 * f;
-	// 	struct wl_buffer *buffer = wp_single_pixel_buffer_manager_v1_create_u32_rgba_buffer(
-	// 		output->state->single_pixel_buffer_manager, r32, g32, b32, a32);
-	// 	wl_surface_attach(output->surface, buffer, 0, 0);
-	// 	wl_surface_damage_buffer(output->surface, 0, 0, INT32_MAX, INT32_MAX);
-
-	// 	struct wp_viewport *viewport = wp_viewporter_get_viewport(
-	// 		output->state->viewporter, output->surface);
-	// 	wp_viewport_set_destination(viewport, output->width, output->height);
-
-	// 	wl_surface_commit(output->surface);
-
-	// 	wp_viewport_destroy(viewport);
-	// 	wl_buffer_destroy(buffer);
-	// 	return;
-	// }
+			output->committed_scale = output->scale;
+		}
+		return;
+	}
 
 	struct pool_buffer buffer;
 	if (!create_buffer(&buffer, output->state->shm,
@@ -163,7 +83,6 @@ static void render_frame(struct swaybg_output *output, cairo_surface_t *surface)
 	int t = time(NULL) % 0xff;
 	int c = t % 2 == 0 ? 0xff0000ff : 0x00ff00ff;
 	cairo_set_source_u32(cairo, c);
-	printf("Rendering\n");
 	cairo_paint(cairo);
 
 	wl_surface_set_buffer_scale(output->surface, output->scale);
@@ -177,23 +96,6 @@ static void render_frame(struct swaybg_output *output, cairo_surface_t *surface)
 
 	// we will not reuse the buffer, so destroy it immediately
 	destroy_buffer(&buffer);
-}
-
-static void destroy_swaybg_image(struct swaybg_image *image) {
-	if (!image) {
-		return;
-	}
-	wl_list_remove(&image->link);
-	free(image);
-}
-
-static void destroy_swaybg_output_config(struct swaybg_output_config *config) {
-	if (!config) {
-		return;
-	}
-	wl_list_remove(&config->link);
-	free(config->output);
-	free(config);
 }
 
 static void destroy_swaybg_output(struct swaybg_output *output) {
@@ -278,17 +180,11 @@ static void create_layer_surface(struct swaybg_output *output) {
 
 static void output_done(void *data, struct wl_output *wl_output) {
 	struct swaybg_output *output = data;
-	if (!output->config) {
-		swaybg_log(LOG_DEBUG, "Could not find config for output %s (%s)",
-				output->name, output->identifier);
-		destroy_swaybg_output(output);
-	} else if (!output->layer_surface) {
-		swaybg_log(LOG_DEBUG, "Found config %s for output %s (%s)",
-				output->config->output, output->name, output->identifier);
+	if (!output->layer_surface) {
+		swaybg_log(LOG_DEBUG, "Output %s (%s)", output->name, output->identifier);
 		create_layer_surface(output);
 	}
 }
-
 static void output_scale(void *data, struct wl_output *wl_output,
 		int32_t scale) {
 	struct swaybg_output *output = data;
@@ -298,28 +194,10 @@ static void output_scale(void *data, struct wl_output *wl_output,
 	}
 }
 
-static void find_config(struct swaybg_output *output, const char *name) {
-	struct swaybg_output_config *config = NULL;
-	wl_list_for_each(config, &output->state->configs, link) {
-		if (strcmp(config->output, name) == 0) {
-			output->config = config;
-			return;
-		} else if (!output->config && strcmp(config->output, "*") == 0) {
-			output->config = config;
-		}
-	}
-}
-
 static void output_name(void *data, struct wl_output *wl_output,
 		const char *name) {
 	struct swaybg_output *output = data;
 	output->name = strdup(name);
-
-	// If description was sent first, the config may already be populated. If
-	// there is an identifier config set, keep it.
-	if (!output->config || strcmp(output->config->output, "*") == 0) {
-		find_config(output, name);
-	}
 }
 
 static void output_description(void *data, struct wl_output *wl_output,
@@ -338,8 +216,6 @@ static void output_description(void *data, struct wl_output *wl_output,
 		}
 		strncpy(output->identifier, description, length);
 		output->identifier[length - 1] = '\0';
-
-		find_config(output, output->identifier);
 	}
 }
 
@@ -400,166 +276,12 @@ static const struct wl_registry_listener registry_listener = {
 	.global_remove = handle_global_remove,
 };
 
-static bool store_swaybg_output_config(struct swaybg_state *state,
-		struct swaybg_output_config *config) {
-	struct swaybg_output_config *oc = NULL;
-	wl_list_for_each(oc, &state->configs, link) {
-		if (strcmp(config->output, oc->output) == 0) {
-			// Merge on top
-			if (config->image_path) {
-				oc->image_path = config->image_path;
-			}
-			if (config->color) {
-				oc->color = config->color;
-			}
-			if (config->mode != BACKGROUND_MODE_INVALID) {
-				oc->mode = config->mode;
-			}
-			return false;
-		}
-	}
-	// New config, just add it
-	wl_list_insert(&state->configs, &config->link);
-	return true;
-}
-
-static void parse_command_line(int argc, char **argv,
-		struct swaybg_state *state) {
-	static struct option long_options[] = {
-		{"color", required_argument, NULL, 'c'},
-		{"help", no_argument, NULL, 'h'},
-		{"image", required_argument, NULL, 'i'},
-		{"mode", required_argument, NULL, 'm'},
-		{"output", required_argument, NULL, 'o'},
-		{"version", no_argument, NULL, 'v'},
-		{0, 0, 0, 0}
-	};
-
-	const char *usage =
-		"Usage: swaybg <options...>\n"
-		"\n"
-		"  -c, --color            Set the background color.\n"
-		"  -h, --help             Show help message and quit.\n"
-		"  -i, --image            Set the image to display.\n"
-		"  -m, --mode             Set the mode to use for the image.\n"
-		"  -o, --output           Set the output to operate on or * for all.\n"
-		"  -v, --version          Show the version number and quit.\n"
-		"\n"
-		"Background Modes:\n"
-		"  stretch, fit, fill, center, tile, or solid_color\n";
-
-	struct swaybg_output_config *config = calloc(sizeof(struct swaybg_output_config), 1);
-	config->output = strdup("*");
-	config->mode = BACKGROUND_MODE_INVALID;
-	wl_list_init(&config->link); // init for safe removal
-
-	int c;
-	while (1) {
-		int option_index = 0;
-		c = getopt_long(argc, argv, "c:hi:m:o:v", long_options, &option_index);
-		if (c == -1) {
-			break;
-		}
-		switch (c) {
-		case 'c':  // color
-			if (!is_valid_color(optarg)) {
-				swaybg_log(LOG_ERROR, "Invalid color: %s", optarg);
-				continue;
-			}
-			config->color = parse_color(optarg);
-			break;
-		case 'i':  // image
-			config->image_path = optarg;
-			break;
-		case 'm':  // mode
-			config->mode = parse_background_mode(optarg);
-			if (config->mode == BACKGROUND_MODE_INVALID) {
-				swaybg_log(LOG_ERROR, "Invalid mode: %s", optarg);
-			}
-			break;
-		case 'o':  // output
-			if (config && !store_swaybg_output_config(state, config)) {
-				// Empty config or merged on top of an existing one
-				destroy_swaybg_output_config(config);
-			}
-			config = calloc(sizeof(struct swaybg_output_config), 1);
-			config->output = strdup(optarg);
-			config->mode = BACKGROUND_MODE_INVALID;
-			wl_list_init(&config->link);  // init for safe removal
-			break;
-		case 'v':  // version
-			fprintf(stdout, "swaybg version " SWAYBG_VERSION "\n");
-			exit(EXIT_SUCCESS);
-			break;
-		default:
-			fprintf(c == 'h' ? stdout : stderr, "%s", usage);
-			exit(c == 'h' ? EXIT_SUCCESS : EXIT_FAILURE);
-		}
-	}
-	if (config && !store_swaybg_output_config(state, config)) {
-		// Empty config or merged on top of an existing one
-		destroy_swaybg_output_config(config);
-	}
-
-	// Check for invalid options
-	if (optind < argc) {
-		config = NULL;
-		struct swaybg_output_config *tmp = NULL;
-		wl_list_for_each_safe(config, tmp, &state->configs, link) {
-			destroy_swaybg_output_config(config);
-		}
-		// continue into empty list
-	}
-	if (wl_list_empty(&state->configs)) {
-		fprintf(stderr, "%s", usage);
-		exit(EXIT_FAILURE);
-	}
-
-	// Set default mode and remove empties
-	config = NULL;
-	struct swaybg_output_config *tmp = NULL;
-	wl_list_for_each_safe(config, tmp, &state->configs, link) {
-		if (!config->image_path && !config->color) {
-			destroy_swaybg_output_config(config);
-		} else if (config->mode == BACKGROUND_MODE_INVALID) {
-			config->mode = config->image_path
-				? BACKGROUND_MODE_STRETCH
-				: BACKGROUND_MODE_SOLID_COLOR;
-		}
-	}
-}
-
 int main(int argc, char **argv) {
 	swaybg_log_init(LOG_DEBUG);
 
 	struct swaybg_state state = {0};
-	wl_list_init(&state.configs);
 	wl_list_init(&state.outputs);
 	wl_list_init(&state.images);
-
-	parse_command_line(argc, argv, &state);
-
-	// Identify distinct image paths which will need to be loaded
-	struct swaybg_image *image;
-	struct swaybg_output_config *config;
-	wl_list_for_each(config, &state.configs, link) {
-		if (!config->image_path) {
-			continue;
-		}
-		wl_list_for_each(image, &state.images, link) {
-			if (strcmp(image->path, config->image_path) == 0) {
-				config->image = image;
-				break;
-			}
-		}
-		if (config->image) {
-			continue;
-		}
-		image = calloc(1, sizeof(struct swaybg_image));
-		image->path = config->image_path;
-		wl_list_insert(&state.images, &image->link);
-		config->image = image;
-	}
 
 	state.display = wl_display_connect(NULL);
 	if (!state.display) {
@@ -592,39 +314,7 @@ int main(int argc, char **argv) {
 						output->layer_surface,
 						output->configure_serial);
 			}
-
-			int buffer_width = output->width * output->scale,
-				buffer_height = output->height * output->scale;
-			bool buffer_change =
-				output->committed_height != buffer_height ||
-				output->committed_width != buffer_width;
-			if (output->dirty && output->config->image && buffer_change) {
-				output->config->image->load_required = true;
-			}
 		}
-
-		// Load images, render associated frames, and unload
-		// wl_list_for_each(image, &state.images, link) {
-		// 	if (!image->load_required) {
-		// 		continue;
-		// 	}
-
-		// 	cairo_surface_t *surface = load_background_image(image->path);
-		// 	if (!surface) {
-		// 		swaybg_log(LOG_ERROR, "Failed to load image: %s", image->path);
-		// 		continue;
-		// 	}
-
-		// 	wl_list_for_each(output, &state.outputs, link) {
-		// 		if (output->dirty && output->config->image == image) {
-		// 			output->dirty = false;
-		// 			render_frame(output, surface);
-		// 		}
-		// 	}
-
-		// 	image->load_required = false;
-		// 	cairo_surface_destroy(surface);
-		// }
 
 		// Redraw outputs without associated image
 		wl_list_for_each(output, &state.outputs, link) {
@@ -638,16 +328,6 @@ int main(int argc, char **argv) {
 	struct swaybg_output *output, *tmp_output;
 	wl_list_for_each_safe(output, tmp_output, &state.outputs, link) {
 		destroy_swaybg_output(output);
-	}
-
-	struct swaybg_output_config *tmp_config = NULL;
-	wl_list_for_each_safe(config, tmp_config, &state.configs, link) {
-		destroy_swaybg_output_config(config);
-	}
-
-	struct swaybg_image *tmp_image;
-	wl_list_for_each_safe(image, tmp_image, &state.images, link) {
-		destroy_swaybg_image(image);
 	}
 
 	return 0;
